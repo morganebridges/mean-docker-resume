0ea632ea44b3a1737b76009aabc42e787835ed2d
Morgan <morganebridges@gmail.com>
2018-04-29 16:00:18 -0500

    added angular app + express api + mongoDb container config

---
+++ angular-client/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  API = 'http://localhost:3000';
+  people: any[] = [];
+
+  //Inject http service through the constructor
+  constructor(private http: Http) {}
+
+  // Angular 2 Life Cycle event when component has been initialized
+  ngOnInit() {
+    this.getAllPeople();
+  }
+  // Add one person to the API
+  addPerson(name, age) {
+    this.http.post(`${this.API}/users`, {name, age})
+      .map(res => res.json())
+      .subscribe(() => {
+        this.getAllPeople();
+      })
+  }
+
+  // Get all users from the API
+  getAllPeople() {
+    this.http.get(`${this.API}/users`)
+      .map(res => res.json())
+      .subscribe(people => {
+        console.log(people)
+        this.people = people
+      })
+  }
+}
